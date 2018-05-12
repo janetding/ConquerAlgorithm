41b36c04b2ce5e8bc0e04da5b443a149a7ec686d
janetding <janetding318@gmail.com>
2018-05-12 15:15:12 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/binarySearch/findMinimumInRotatedSortedArrayLC153.java

--- src/binarySearch/powxnLC50.java
+++ src/binarySearch/powxnLC50.java
@@ -1,10 +1,16 @@
 package binarySearch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
 /**
  * Created by janet1 on 5/12/18.
  */
 public class powxnLC50 {
     public double myPow(double x, int n){
+
         if (n == Integer.MIN_VALUE){
             double half = myPow(x, n >> 1);
             return half * half;
@@ -15,6 +21,24 @@ public class powxnLC50 {
         double half = myPow(x, n >> 1);
         return n % 2 == 0 ? half * half : half * half * x;
     }
+
+    public static String doubleToString(double input) {
+        return new DecimalFormat("0.00000").format(input);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            double x = Double.parseDouble(line);
+            line = in.readLine();
+            int n = Integer.parseInt(line);
+
+            double ret = new powxnLC50().myPow(x, n);
+
+            String out = doubleToString(ret);
+
+            System.out.print(out);
+        }
+    }
 }
-
-
