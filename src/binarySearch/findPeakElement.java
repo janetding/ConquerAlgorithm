761ceffe61b9dd8bf136e2f5cbb556861a2bcbc8
janetding <janetding@users.noreply.github.com>
2018-06-05 00:24:52 -0700

    Update findPeakElement.java

--- src/binarySearch/findPeakElement.java
+++ src/binarySearch/findPeakElement.java
@@ -4,29 +4,34 @@ package binarySearch;
  * Created by janet1 on 5/10/18.
  */
 public class findPeakElement {
-    public static int findPeakElementProb(int[] nums, int n) {
-        //corner case
-        if (nums == null || nums.length == 0) return -1;
-        int start = 0; n = nums.length;
-        int end = n - 1, mid;
-        while (start + 1 < end) {
-            mid = start + ((end - start) >> 1);
-            int pre = Math.max(0, mid - 1);
-            int post = Math.min(mid + 1, n);
-            if (nums[mid] > nums[pre] && nums[mid] > nums[post]) return mid;
-            else if (nums[mid] > nums[pre] && nums[mid] < nums[post]) start = mid + 1;
-            else end = mid - 1;
+ public static int findPeakElementProb(int[] nums) {
+            //corner case
+            if ( nums == null || nums.length == 0) return -1;
+            int start = 0; int end =nums.length -1; int mid;
+            while(start + 1 < end) {
+                mid = start + ((end - start) >> 1);
+                if (nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) {
+                    return mid;
+                }else if(nums[mid] < nums[mid +1] && nums[mid] > nums[mid-1]){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+
+            }
+            return nums[start] > nums[end] ? start : end;
         }
-        if (nums[start] > nums[end]) return start;
-        return end;
-    }
+
+
+
 
     public static void main(String[] args) {
         int arr[] = {5, 6, 7, 8, 9, 10,4};
-        int n,key;
-        n = arr.length;
-        System.out.println("Index: " + findPeakElementProb(arr,n) );
+
+        System.out.println("Index: " + findPeakElementProb(arr) );
     }
 }
+
+
 
 
