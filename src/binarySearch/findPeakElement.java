e813302eeb5189267aa84944eca9c9a021e04004
janetding <janetding@users.noreply.github.com>
2018-06-05 00:35:45 -0700

    Update findPeakElement.java

--- src/binarySearch/findPeakElement.java
+++ src/binarySearch/findPeakElement.java
@@ -21,6 +21,41 @@ public class findPeakElement {
             }
             return nums[start] > nums[end] ? start : end;
         }
+ /**
+ *using BST to get valley number
+ *To the opporsite way
+ * Heights[left] , Heights[right] ,which one is smaller, choose that one
+ public static int findOneValley(int[] heights) {
+			
+			int left =0,  right = heights.length - 1; 
+			
+			//try{
+				if (heights == null || heights.length == 0) {
+					throw new UnsupportedOperationException("Not implemented");
+				}
+				
+				while (left + 1 < right){
+					int mid = left + ((right - left) >> 1);
+					if (heights[mid] < heights[mid+1] && heights[mid] < heights[mid-1]) {
+						return mid;
+					}
+					else if (heights[mid] < heights[mid+1] && heights[mid] > heights[mid-1])  {
+						right = mid;
+					}
+					else {
+						left = mid;
+					}
+				}
+				
+				if(heights[left] < heights[right]){
+					return left;
+				}
+				else if(heights[left] > heights[right]){
+					return right;
+				}
+				else{
+					return -1;
+				}*/			
 
 
 
